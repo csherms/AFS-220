80aec231d4ee1abe9756838e3268f2721da634b8
csherms <casey.sherman@bryanuniversity.edu>
2022-04-14 18:37:19 -0600

    starting_adding_in_some_styling

--- week-2/Netflix_Clone/netflix_clone/src/App.js
+++ week-2/Netflix_Clone/netflix_clone/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-
+import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
-
+import Banner from "./Banner";
 import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className='app'>
       <Navbar />
-
+      <Banner />
       <Row
         title='NETFLIX ORIGINALS'
         fetchUrl={requests.fetchNetflixOriginals}
